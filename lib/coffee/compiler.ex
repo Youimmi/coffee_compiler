f2fafc19a38a463df2d5db14ff468c585395fa67
Yuri S <fudoshiki.ari@gmail.com>
2020-12-20 10:10:51 +0500

    Fix yarn path

--- lib/coffee/compiler.ex
+++ lib/coffee/compiler.ex
@@ -3,9 +3,15 @@ defmodule Coffee.Compiler do
   Compile coffee source to javascript source
   """
 
-  def compile(coffee, _opts) do
+  def compile(coffee, opts) do
+    assets_path =
+      opts
+      |> Map.new()
+      |> Map.get(:assets_path, "assets")
+
     config = Application.app_dir(:coffee_compiler, "priv/rollup.config.js")
-    {js, _} = System.cmd("yarn", ["run", "rollup", coffee, "-c", config])
+
+    {js, _} = System.cmd("yarn", ["run", "rollup", coffee, "-c", config], cd: assets_path)
     {:ok, js}
   end
 end
